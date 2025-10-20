608dea009823ebb8c81cb380c130946c4aa949a7
Bollaboina Vamshi Yadav <130643624+vamshiyadavbollaboina@users.noreply.github.com>
2025-10-20 17:36:27 +0530

    Update constants.js

--- server/src/constants.js
+++ server/src/constants.js
@@ -2,7 +2,7 @@ require('dotenv');
 
 const auth = {
     type: 'OAuth2',
-    user: 'shraddha.gawde1999@gmail.com',
+    user: 'vamshiyadavbollaboina@gmail.com',
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     refreshToken: process.env.GOOGLE_REFRESH_TOKEN
